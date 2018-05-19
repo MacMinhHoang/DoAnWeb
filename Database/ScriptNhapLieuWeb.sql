3260762b8417d264861cd93955e0966ee5cce614
Huy <nguyenlehuy1101@gmail.com>
2018-05-20 03:15:43 +0700

    sửa lại script nhập liệu

--- Database/ScriptNhapLieuWeb.sql
+++ Database/ScriptNhapLieuWeb.sql
@@ -1,21 +1,44 @@
-INSERT INTO khachhang (ID,HoTen,NgaySinh,DiaChi,SDT,Email,TenDangNhap,MatKhau,GioiTinh) VALUES 
-('1', 'Nguyễn Lê Huy', '1997-01-11', '54 Lê Quang Định', '01648002334', 'ab@gmail.com', 'huynl', 'yaoming2010', '1')
+INSERT INTO khachhang (HoTen,NgaySinh,DiaChi,SDT,Email,TenDangNhap,MatKhau,GioiTinh) VALUES 
+('Nguyễn Lê Huy', '1997-01-11', '54 Lê Quang Định', '01648002334', 'ab@gmail.com', 'huynl', 'yaoming2010', '1')
 
-INSERT INTO khachhang (ID,HoTen,NgaySinh,DiaChi,SDT,Email,TenDangNhap,MatKhau,GioiTinh) VALUES ('2', 'Mạc Minh Hoàng', '1997-04-15', '133/5 Trần Phú', '01632348754', 'hoang@gmail.com', 'hoangmm', 'minhhoang', '0')
+INSERT INTO khachhang (HoTen,NgaySinh,DiaChi,SDT,Email,TenDangNhap,MatKhau,GioiTinh) VALUES 
+('Mạc Minh Hoàng', '1997-04-15', '133/5 Trần Phú', '01632348754', 'hoang@gmail.com', 'hoangmm', 
+'minhhoang', '0')
 
 
-INSERT INTO khachhang (ID,HoTen,NgaySinh,DiaChi,SDT,Email,TenDangNhap,MatKhau,GioiTinh) VALUES ('3', 'Đỗ Huy Gia Cát', '1997-07-12', '12/4/5 Lương Ngọc Quyến', '0903567410', 'cat@gmail.com', 'catdhg', 'cat123', '1')
+INSERT INTO khachhang (HoTen,NgaySinh,DiaChi,SDT,Email,TenDangNhap,MatKhau,GioiTinh) VALUES
+ ('Đỗ Huy Gia Cát', '1997-07-12', '12/4/5 Lương Ngọc Quyến', '0903567410', 'cat@gmail.com', 'catdhg',
+ 'cat123', '1')
 
-INSERT INTO donhang (ID,KhachHang,ThoiGian,TongTien,TrangThai) VALUES 
-('100001','1','2018-05-14','11490000','0');
+INSERT INTO donhang (KhachHang,ThoiGian,TongTien,TrangThai) VALUES 
+('11111001','2018-05-14','11490000','0');
 
-INSERT INTO donhang (ID,KhachHang,ThoiGian,TongTien,TrangThai) VALUES 
-('100002','1','2018-02-05','10390000','2');
+INSERT INTO donhang (KhachHang,ThoiGian,TongTien,TrangThai) VALUES 
+('11111002','2018-02-05','10390000','2');
 
-INSERT INTO donhang (ID,KhachHang,ThoiGian,TongTien,TrangThai) VALUES 
-('100003','3','2018-03-07','115000000','1');
+INSERT INTO donhang (KhachHang,ThoiGian,TongTien,TrangThai) VALUES 
+('11111003','2018-03-07','115000000','1');
 
-delete from donhang where ID = '100003'
+INSERT INTO donhang (KhachHang,ThoiGian,TongTien,TrangThai) VALUES 
+('11111002','2016-06-01','15200000','0');
+
+INSERT INTO donhang (KhachHang,ThoiGian,TongTien,TrangThai) VALUES 
+('11111003','2017-01-24','40000000','1');
+
+INSERT INTO donhang (KhachHang,ThoiGian,TongTien,TrangThai) VALUES 
+('11111001','2014-11-21','9000000','0');
+
+INSERT INTO donhang (KhachHang,ThoiGian,TongTien,TrangThai) VALUES 
+('11111001','2015-12-04','12000000','2');
+
+INSERT INTO donhang (KhachHang,ThoiGian,TongTien,TrangThai) VALUES 
+('11111002','2016-04-07','105000000','0');
+
+INSERT INTO donhang (KhachHang,ThoiGian,TongTien,TrangThai) VALUES 
+('11111001','2017-12-15','5000000','1');
+
+
+
 
 INSERT INTO loai (ID, TenLoai) VALUES ('1','Super-zoom Camera');
 
@@ -27,48 +50,103 @@ INSERT INTO loai (ID, TenLoai) VALUES ('4','Travel Camera');
 
 INSERT INTO loai (ID, TenLoai) VALUES ('5','Waterproof Camera');
 
-INSERT INTO nhasanxuat (ID, TenNSX) VALUES ('1','Canon');
+INSERT INTO loai (ID, TenLoai) VALUES ('6','Others');
 
-INSERT INTO nhasanxuat (ID, TenNSX) VALUES ('2','Dahua');
+INSERT INTO nhasanxuat (TenNSX) VALUES ('Canon');
 
-INSERT INTO nhasanxuat (ID, TenNSX) VALUES ('3','Fujifilm');
+INSERT INTO nhasanxuat (TenNSX) VALUES ('Dahua');
 
-INSERT INTO nhasanxuat (ID, TenNSX) VALUES ('4','GoPro');
+INSERT INTO nhasanxuat (TenNSX) VALUES ('Fujifilm');
 
-INSERT INTO nhasanxuat (ID, TenNSX) VALUES ('5','Logitech');
+INSERT INTO nhasanxuat (TenNSX) VALUES ('GoPro');
 
-INSERT INTO nhasanxuat (ID, TenNSX) VALUES ('6','Nikon');
+INSERT INTO nhasanxuat (TenNSX) VALUES ('Logitech');
 
-INSERT INTO nhasanxuat (ID, TenNSX) VALUES ('7','Olympus');
+INSERT INTO nhasanxuat (TenNSX) VALUES ('Nikon');
 
-INSERT INTO nhasanxuat (ID, TenNSX) VALUES ('8','Pentax');
+INSERT INTO nhasanxuat (TenNSX) VALUES ('Olympus');
 
-INSERT INTO nhasanxuat (ID, TenNSX) VALUES ('9','Sony');
+INSERT INTO nhasanxuat (TenNSX) VALUES ('Pentax');
 
-INSERT INTO sanpham (ID,Ten,HinhAnh,GiaBan,SoLuotXem,SoLuongTon,SoLuongBan,MoTa,Loai,NhaSanXuat,NgayTiepNhan)
-VALUES ('00001','CANON EOS 5D MARK III KIT EF 24-105 F4L IS USM','/products/Canon/[2].CANON EOS 5D MARK 
+INSERT INTO nhasanxuat (TenNSX) VALUES ('Sony');
+
+INSERT INTO sanpham (Ten,HinhAnh,GiaBan,SoLuotXem,SoLuongTon,SoLuongBan,MoTa,Loai,NhaSanXuat,NgayTiepNhan)
+VALUES ('CANON EOS 5D MARK III KIT EF 24-105 F4L IS USM','/products/Canon/[2].CANON EOS 5D MARK 
 III KIT EF 24-105 F4L IS USM/2-3.jpg','57500000','5240','920','523','Khả năng chụp
  liên tục 950 tấm ảnh chỉ với 1 lần sạc pin, máy ảnh Canon EOS 5D Mark III là sự lựa chọn hoàn hảo trong tầm
  giá cho bất cứ ai. Bên cạnh đó, bộ xử lý hình ảnh được nần cấp mạnh hơn 17 lần so với đàn anh 5D Mark II là
- một điểm đáng chú ý của sản phẩm này.','3','1','2017-12-7');
+ một điểm đáng chú ý của sản phẩm này.','3','33333001','2017-12-7');
 
-INSERT INTO sanpham (ID,Ten,HinhAnh,GiaBan,SoLuotXem,SoLuongTon,SoLuongBan,MoTa,Loai,NhaSanXuat,NgayTiepNhan)
-VALUES ('00002','FUJIFILM X-A3 KIT XC16-50 F3.5-5.6 OIS II (NÂU)','/products/Fujifilm/FUJIFILM_ X-A3-Nau.jpg'
+INSERT INTO sanpham (Ten,HinhAnh,GiaBan,SoLuotXem,SoLuongTon,SoLuongBan,MoTa,Loai,NhaSanXuat,NgayTiepNhan)
+VALUES ('FUJIFILM X-A3 KIT XC16-50 F3.5-5.6 OIS II (NÂU)','/products/Fujifilm/FUJIFILM_ X-A3-Nau.jpg'
 ,'10390000','1462','45','14','Người dùng vẫn yêu thích sự hoài cổ trong từng đường nét, và  máy ảnh Fujifilm
  X-A3 vẫn làm say đắm với sự cổ điển vốn có từ đàn anh X-A2. Các màu sắc như hồng, mâu và đen như những chiếc 
- ao mới, làm nổi bật thêm sự độc đáo vốn đã có của cái tên đình đám.','4','3','2015-05-04');
+ ao mới, làm nổi bật thêm sự độc đáo vốn đã có của cái tên đình đám.','4','33333003','2015-05-04');
  
- INSERT INTO sanpham (ID,Ten,HinhAnh,GiaBan,SoLuotXem,SoLuongTon,SoLuongBan,MoTa,Loai,NhaSanXuat,NgayTiepNhan)
-VALUES ('00003','OLYMPUS STYLUS TOUGH TG-5','/products/Olympus/OLYMPUS STYLUS TOUGH TG-5/1.jpg','11490000','528
-','421','233','Nhà sản xuất máy ảnh đình đám với các sản phẩm "nồi đồng cối đá" Olympus mới đây vừa tung ra 
+ INSERT INTO sanpham (Ten,HinhAnh,GiaBan,SoLuotXem,SoLuongTon,SoLuongBan,MoTa,Loai,NhaSanXuat,NgayTiepNhan)
+VALUES ('OLYMPUS STYLUS TOUGH TG-5','/products/Olympus/OLYMPUS STYLUS TOUGH TG-5/1.jpg','11490000','528','421','233','Nhà sản xuất máy ảnh đình đám với các sản phẩm "nồi đồng cối đá" Olympus mới đây vừa tung ra 
 mẫu máy ảnh Tough TG-5. Sản phẩm trang bị cảm biến thế hệ mới cùng khả năng quay video 4K đầy mạnh mẽ.
 Theo Petapixel, Olympus Tough TG-5 sở hữu cảm biến thế hệ mới, bộ vi xử lý nâng cấp và tính năng quay video 4K.
  Ngoài ra, nhà sản xuất cũng cung cấp một bộ vỏ bọc chống nước, chống shock cũng như chống ăn mòn và bụi bẩn.',
- '5','7','2016-12-01');
+ '5','33333007','2016-12-01');
  
-INSERT INTO sp_dh (ID,DonHang,SanPham,SoLuong) VALUES ('1','100002','2','1');
+INSERT INTO sanpham (Ten,HinhAnh,GiaBan,SoLuotXem,SoLuongTon,SoLuongBan,MoTa,Loai,NhaSanXuat,NgayTiepNhan)
+VALUES ('Camera IP Wifi Dahua 1.3Mp IPC-A15P','/products/Dahua/dahua_3.jpg','1690000','105','45','12','amera Dahua
+ DH-IPC-A15P thiết kế chắc chắn, nhỏ gọn, thẩm mỹ cao với nhiều tính năng công nghệ hồng ngoại thông minh, phù
+ hợp sử dụng cho gia đình, văn phòng, cửa hàng. Với thế mạnh là dòng camera ip không dây "Home-use Series" 
+ của DAHUA, cho chất lượng hình ảnh nổi bật với độ phân giải cao (1.3 megapixel), hỗ trợ míc và loa, đàm thoại
+ 2 chiều, quan sát ngày đêm tốt trong khoảng cách 10 mét. Đặc biệt, sản phẩm này còn hỗ trợ khả năng quay quét
+ ngang (PAN)  355° tốc độ 100° /s, quay dọc lên xuống 90° 100° /s, hỗ trợ cài đặt trước 25 điểm, 8 hành trình
+ (Tour).','6','33333002','2009-04-11');
  
-INSERT INTO sp_dh (ID,DonHang,SanPham,SoLuong) VALUES ('2','100001','3','1');
  
-INSERT INTO sp_dh (ID,DonHang,SanPham,SoLuong) VALUES ('3','100003','1','2');
+INSERT INTO sanpham (Ten,HinhAnh,GiaBan,SoLuotXem,SoLuongTon,SoLuongBan,MoTa,Loai,NhaSanXuat,NgayTiepNhan)
+VALUES ('GoPro Fusion','/products/GoPro/goproFUSION.jpg','17990000','245','40','5','Theo Slashgear, GoPro Fusion là mẫu
+ action cam 360 độ mới nhất của GoPro, hỗ trợ quay video 5.2K tốc độ 30fps hoặc 3K 60fps. Máy cũng có thể chụp 
+ ảnh tĩnh với độ phân giải 18MP, tính năng ổn định và chống rung kỹ thuật số. Đặc biệt nhờ hỗ trợ âm thanh 360
+ độ, máy sẽ tạo trải nghiệm video liền mạch và ấn tượng hơn.','6','33333004','2017-09-18');
+ 
+  
+INSERT INTO sanpham (Ten,HinhAnh,GiaBan,SoLuotXem,SoLuongTon,SoLuongBan,MoTa,Loai,NhaSanXuat,NgayTiepNhan)
+VALUES ('CANON IXUS 185 (ĐEN)','/products/Canon/[8].CANON IXUS 185 (ĐEN)/8-3.jpg','2350000','1502','400','240',
+'Máy ảnh Canon IXUS 185 kết hợp cảm biến 20.0 MP cùng bộ xử lý ảnh DIGIC 4+ góp phần không nhỏ mang đến cho
+ bạn những bức ảnh với màu sắc đẹp và các chi tiết rõ nét ở mức tối đa. Bên cạnh đó, dải ISO ở mức 100 
+ - 800 sẽ hỗ trợ rất tốt ở những nơi mà bạn chụp ảnh thiếu sáng.','4','33333001','2016-11-12');
+ 
+ 
+ INSERT INTO sanpham (Ten,HinhAnh,GiaBan,SoLuotXem,SoLuongTon,SoLuongBan,MoTa,Loai,NhaSanXuat,NgayTiepNhan)
+VALUES ('SONY CYBERSHOT DSC H300 (ĐEN)','/products/Sony/SONY_CYBERSHOT-DSC-H300.jpg','3450000','1042','150','63',
+'Máy ảnh Sony Cybershot DSC H300 là chiếc máy ảnh du lịch được thiết kế với kiểu dáng hầm hố theo phong cách 
+của những chiếc máy ảnh DSLR. Chiếc máy ảnh này sẽ mang lại cho bạn những trải nghiệm tuyệt vời với khả năng
+ chụp ảnh đa dạng. Hãy để những khoảnh khắc ấn tượng nằm trong tầm tay bạn.','1','33333009','2015-07-24');
+ 
+ INSERT INTO sanpham (Ten,HinhAnh,GiaBan,SoLuotXem,SoLuongTon,SoLuongBan,MoTa,Loai,NhaSanXuat,NgayTiepNhan)
+VALUES ('NIKON D5600 KIT AF-P 18-55 VR','/products/Nikon/NIKON_D5600.jpg','15900000','423','100','5','Máy ảnh
+ Nikon D5600 là mẫu máy ảnh được sản xuất nhằm khắc phục những thiếu sót mà người đàn anh D5500 để lại. Với
+ những tính năng vượt trội cùng với những khả năng kết nối và chia sẽ thông minh, chiếc máy ảnh DSLR Nikon mới
+ này sẽ đáp ứng cho nhu cầu của người dùng một cách đầy đủ nhất, mang lại những hình ảnh sống động trong cuộc
+ sống hàng ngày của bạn','6','33333006','2017-03-15');
+ 
+ 
+INSERT INTO sanpham (Ten,HinhAnh,GiaBan,SoLuotXem,SoLuongTon,SoLuongBan,MoTa,Loai,NhaSanXuat,NgayTiepNhan)
+VALUES ('SONY ALPHA A6000 (ILCE-6000L) KIT 16-50 F3.5-5.6 OSS (ĐEN)','/products/Sony/SONY_ALPHA-A6000.jpg','13490000',
+'760','250','124','Máy ảnh Sony Alpha A6000 là sự cải tiến vượt bậc của Sony với khoảng thời gian lấy nét chỉ 
+trong 0.06s. Ngoài ra, những hình ảnh với dải màu sâu và độ tương phản mềm mại sẽ được tạo ra liên tục với hơn
+ 600 tấm ảnh chỉ trong một lần sạc pin. Máy được trang bị bộ cảm biến Exmor APS HD CMOS 24.3MP APS-C và bộ
+ xử lý hình ảnh BIONZ X có độ phân giải cao rất có lợi để chụp những ảnh khổ lớn chuyên dụng cho thiết kế hay
+ những chi tiết dù nhỏ nhất cũng sẽ được ghi lại qua từng điểm ảnh một các rõ nét nhất. Các chip trên ống kính
+ lấp đầy những khoảng trống giữa các điểm ảnh lân cận nhằm phù hợp với khả năng tập trung ánh sáng và độ sắc 
+ nét.','2','33333009','2014-12-09');
+
+
+INSERT INTO sanpham (Ten,HinhAnh,GiaBan,SoLuotXem,SoLuongTon,SoLuongBan,MoTa,Loai,NhaSanXuat,NgayTiepNhan)
+VALUES ('FUJIFILM X-A3 KIT XC16-50MM F3.5-5.6 OIS II (BẠC)','/products/Fujifilm/FUJIFILM_X-A3-Bac.jpg','10390000',
+'503','90','54','Vẫn giữ nét đặc trưng hoài cổ của mình nhưng lại được khoác lên những lớp áo mới thời trang hơn, nổi bật hơn, máy ảnh Fujifilm X-A3 Kit XC16-50 OIS II + XC50-230 OIS II (Bạc) 
+chính là sản phẩm mới nhất trong phân khúc giá rẻ mà Fujifilm dành cho tất cả mọi người.','4','33333003','2015-11-30');
+
+INSERT INTO sp_dh (DonHang,SanPham,SoLuong) VALUES ('123456002','789654002','1');
+ 
+INSERT INTO sp_dh (DonHang,SanPham,SoLuong) VALUES ('123456007','789654004','1');
+ 
+INSERT INTO sp_dh (DonHang,SanPham,SoLuong) VALUES ('123456005','789654011','2');
  
