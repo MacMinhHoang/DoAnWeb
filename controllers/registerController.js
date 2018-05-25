2ba254c9dd6e5cd484c8329ea3e5a68f4358bd75
MAC MINH HOANG <1512176@student.hcmus.edu.vn>
2018-05-25 10:25:29 +0700

    Sửa lỗi css và javascript

--- controllers/registerController.js
+++ controllers/registerController.js
@@ -6,8 +6,4 @@ router.get('/', (req, res) => {
     res.render('register/register_page');
 });
 
-router.get('/captcha_form', (req, res) => {
-    res.render('register/captcha_form');
-});
-
 module.exports = router;
