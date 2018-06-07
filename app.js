5a9dee60d04e8f29ae0dbf26c873a8df9967b8f2
MAC MINH HOANG <1512176@student.hcmus.edu.vn>
2018-06-07 20:07:10 +0700

    Tạo sẵn các controller, repo

--- app.js
+++ app.js
@@ -3,6 +3,10 @@ var exphbs = require('express-handlebars');
 var express_handlebars_sections = require('express-handlebars-sections');
 var bodyParser = require('body-parser');
 var path = require('path');
+var wnumb = require('wnumb');
+
+var session = require('express-session');
+var MySQLStore = require('express-mysql-session')(session);
 
 var handleLayoutMDW = require('./middle-wares/handleLayout'),
     handle404MDW = require('./middle-wares/handle404');
@@ -18,7 +22,13 @@ app.engine('hbs', exphbs({
     defaultLayout: 'main',
     layoutsDir: 'views/_layouts/',
     helpers: {
-        section: express_handlebars_sections()
+        section: express_handlebars_sections(),
+        number_format: n => {
+            var nf = wnumb({
+                thousand: ','
+            });
+            return nf.to(n);
+        }
     }
 }));
 app.set('view engine', 'hbs');
@@ -28,6 +38,32 @@ app.use(express.static(path.resolve(__dirname, 'public')));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
     extended: false
+}));
+
+
+var sessionStore = new MySQLStore({
+    host: 'localhost',
+    port: 3306,
+    user: 'root',
+    password: 'hoang12124',
+    database: 'mystoredb',
+    createDatabaseTable: true,
+    schema: {
+        tableName: 'sessions',
+        columnNames: {
+            session_id: 'session_id',
+            expires: 'expires',
+            data: 'data'
+        }
+    }
+});
+
+app.use(session({
+    key: 'session_cookie_name',
+    secret: 'session_cookie_secret',
+    store: sessionStore,
+    resave: false,
+    saveUninitialized: false
 }));
 
 
---
+++ controllers/productController.js
@@ -0,0 +1,5 @@
+var express = require('express');
+// var productRepo = require('../repos/productRepo');
+var config = require('../config/config');
+
+var router = express.Router();
