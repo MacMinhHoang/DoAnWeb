36f25d9a867b2165c76ae3da2d8e225d1c622258
Huy <nguyenlehuy1101@gmail.com>
2018-06-11 21:20:06 +0700

    Thêm, chỉnh sửa sản phẩm + thêm hình cho sản phẩm
    Chỉnh sửa 1 sản phẩm Dahua, thêm 1 sản phẩm sony vào CSDL và vào trang professional.html. Thêm hình cho tất cả sản phẩm

---
+++ Database/FixCameraDahua.sql
@@ -0,0 +1,18 @@
+
+
+
+update sanpham set Ten="Camera IP Wifi DAHUA IPC-C10", HinhAnh ="/products/Dahua/dahua-4.jpg",GiaBan = "2600000",
+  SoLuotXem = "158",SoLuongTon = "70",SoLuongBan = "42",MoTa="Camera IP Wifi là dòng sản phẩm mới với thiết kế
+  nhỏ  gọn tính năng vượt trội không cần sử dụng qua đầu ghi hình ,âm thanh hai chiều, hình ảnh sắc nét cài đặt
+  sử   dụng đơn giản bạn có thể tự lắp đặt hoàn toàn không dây rất phù hợp với gia đình, văn phòng, công ty...vv...",
+  Loai = "6",NhaSanXuat = "33333002",NgayTiepNhan = "2015-07-11" where ID="789654021"
+  
+  
+INSERT INTO sanpham (Ten,HinhAnh,GiaBan,SoLuotXem,SoLuongTon,SoLuongBan,MoTa,Loai,NhaSanXuat,NgayTiepNhan)
+VALUES ('MÁY ẢNH SONY CYBERSHOT DSC W830 (ĐEN)','/products/Sony/SONY_CYBERSHOT-DSC-W830.jpg','2550000','752','150','92',
+'Máy ảnh Sony DSC-W830 là một máy ảnh nhỏ gọn với 20.1MP , 1/2.3 " Super HAD CCD cảm biến hình ảnh và bộ xử lý BIONZ
+ cho độ phân giải hình ảnh cao nhất với HD 1280 x 720 quay video 30 fps , và sự nhạy cảm ánh sáng tiêu chuẩn ISO 3200
+ . các zoom quang 8x, 4,5 -36mm , ống kính Carl Zeiss Vario-Tessar cung cấp một loạt 35mm tương đương tiêu cự 25 -200mm
+ , bao gồm góc và một loạt các khẩu độ f/3.3-6.3','1','33333009','2017-12-04');
+  
+  
--- app.js
+++ app.js
@@ -42,11 +42,28 @@ app.use(bodyParser.urlencoded({
 }));
 
 
+// var sessionStore = new MySQLStore({
+//     host: 'localhost',
+//     port: 3306,
+//     user: 'root',
+//     password: 'hoang12124',
+//     database: 'mystoredb',
+//     createDatabaseTable: true,
+//     schema: {
+//         tableName: 'sessions',
+//         columnNames: {
+//             session_id: 'session_id',
+//             expires: 'expires',
+//             data: 'data'
+//         }
+//     }
+// });
+
 var sessionStore = new MySQLStore({
     host: 'localhost',
     port: 3306,
     user: 'root',
-    password: 'hoang12124',
+    password: 'yaoming2010',
     database: 'mystoredb',
     createDatabaseTable: true,
     schema: {
