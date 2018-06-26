4e45234b410293e2be0a3b989d56dfa97a77e63d
Huy <nguyenlehuy1101@gmail.com>
2018-06-26 12:20:12 +0700

    bổ sung csdl

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,10 +1,5 @@
 var express = require('express');
-<<<<<<< HEAD
 //var config = require('../config/config');
-=======
-// var config = require('../config/config');
->>>>>>> a356c96e228131b9c8149caa528ee45994a7fd84
-
 var searchRepo = require('../repos/searchRepo');
 var productRepo = require('../repos/productRepo');
 var manuRepo = require('../repos/manufacturerRepo');
